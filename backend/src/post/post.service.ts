ff8f31d66c4122d1661bd46144bc4699d7f58b90
razdor <oneamaday@gmail.com>
2024-07-08 00:07:45 +0300

    fix post service

--- backend/src/post/post.service.ts
+++ backend/src/post/post.service.ts
@@ -1,42 +1,34 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DataSource, EntityManager, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Post } from './entities/post.entity';
 import { CreatePostDto } from './dto/create-post.dto';
 import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class PostService {
+  private readonly logger = new Logger(PostService.name);
+
   constructor(
     @InjectRepository(Post)
     private readonly postRepository: Repository<Post>,
     @InjectRepository(User)
-    private userRepository: Repository<User>,
-    private dataSource: DataSource,
+    private readonly userRepository: Repository<User>,
   ) {}
 
   async findOne(id: number): Promise<Post> {
-    const queryRunner = this.dataSource.createQueryRunner();
-
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
-
-    try {
-      const post = await queryRunner.manager.findOne(Post, { where: { id } });
-      if (!post) {
-        throw new NotFoundException('Post not found');
-      }
-
-      await this.incrementViewCount(id, queryRunner.manager);
-
-      await queryRunner.commitTransaction();
-      return post;
-    } catch (err) {
-      await queryRunner.rollbackTransaction();
-      throw err;
-    } finally {
-      await queryRunner.release();
+    const result = await this.postRepository.createQueryBuilder('post')
+      .where('post.id = :id', { id })
+      .update()
+      .set({ views: () => 'views + 1' })
+      .returning('*')  
+      .execute();
+    
+    if (result.affected === 0) {
+      throw new NotFoundException('Post not found');
     }
+  
+    return result.raw[0];  
   }
 
   async findAll(): Promise<Post[]> {
@@ -49,16 +41,8 @@ export class PostService {
     if (!author) {
       throw new NotFoundException('User not found');
     }
-  
+
     const newPost = this.postRepository.create({ content, author });
     return this.postRepository.save(newPost);
-  }
-
-  private async incrementViewCount(id: number, manager: EntityManager): Promise<void> {
-    await manager.createQueryBuilder()
-      .update(Post)
-      .set({ views: () => 'views + 1' })
-      .where('id = :id', { id })
-      .execute();
   }
 }
